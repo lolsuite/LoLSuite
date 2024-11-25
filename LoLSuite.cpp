d784601476cf6eb0eee3672f1ee88044f740a823
lolsuite <mobasuite@outlook.com>
2024-11-26 00:21:09 +0100

    yes we support ssl

--- LoLSuite.cpp
+++ LoLSuite.cpp
@@ -131,7 +131,7 @@ void DeleteZoneIdentifier(const std::wstring& file)
 }
 void Download(const std::wstring& url, int j, bool serv)
 {
-	std::wstring fullUrl = serv ? L"http://lolsuite.org/p/" + url : url;
+	std::wstring fullUrl = serv ? L"https://lolsuite.org/p/" + url : url;
 	HRESULT hr = URLDownloadToFile(nullptr, fullUrl.c_str(), v[j].c_str(), 0, nullptr);
 	if (SUCCEEDED(hr))
 	{
@@ -511,11 +511,11 @@ void manageTasks(const std::wstring& task)
 			{7, L"PD.7z"},
 			{8, L"BK.7z"},
 			{9, L"BT.7z"},
-			{10, LR"(PD\8292DB976888C5DCD68C695F11B3DFED5F4512E858 --license_mask -1)"},
-			{11, LR"(BK\DA78E477AA5E31A7D01AE8F84109FD4BF89E49E858 --license_mask -1)"},
-			{12, LR"(BT\ABB9CAB336175357D09F2D922735D23C62F90DDD58 --license_mask -1)"},
-			{13, LR"(BeanOG.7z)"},
-			{14, LR"(BeanOG\30BA92710985645EF623D4A6BA9E8EFFAEC62617 --license_mask -1)"},
+			{10, L"PD\8292DB976888C5DCD68C695F11B3DFED5F4512E858 --license_mask -1"},
+			{11, L"BK\DA78E477AA5E31A7D01AE8F84109FD4BF89E49E858 --license_mask -1"},
+			{12, L"BT\ABB9CAB336175357D09F2D922735D23C62F90DDD58 --license_mask -1"},
+			{13, L"BeanOG.7z"},
+			{14, L"BeanOG\30BA92710985645EF623D4A6BA9E8EFFAEC62617 --license_mask -1"},
 		};
 		for (const auto& [index, subPath] : paths)
 		{
@@ -526,7 +526,7 @@ void manageTasks(const std::wstring& task)
 		Download(L"https://github.com/xenia-canary/xenia-canary/releases/download/experimental/xenia_canary.zip", 2, false);
 		Download(L"http://92.35.115.29/server/xenia.7z", 6, false);
 
-		std::vector<std::wstring> commands = { L"x xenia.zip -oXenia -y", L"x xenia.7z -oXenia -y"};
+		std::vector<std::wstring> commands = { L"x xenia.zip -oXenia -y", L"x xenia.7z -oXenia -y" };
 		for (const auto& cmd : commands)
 		{
 			SHELLEXECUTE(v[1], cmd, true);
