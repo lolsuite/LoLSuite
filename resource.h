8611e566a5944093291f6e38062eff9b57d3f355
lolsuite <mobasuite@outlook.com>
2024-11-26 17:59:56 +0100

    version info

--- resource.h
+++ resource.h
@@ -1,8 +1,14 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LoLSuite.rc
+//
 #define IDS_APP_TITLE                   100
 #define IDM_EXIT                        101
 #define IDI_ICON                        102
 #define IDC_BUFFER                      103
 
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
