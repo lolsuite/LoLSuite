b465f5835aca6d9c118bfbf84276d52d1fa0f7cf
lolsuite <mobasuite@outlook.com>
2024-12-23 15:52:21 +0100

    Update LoLSuite.cpp

--- LoLSuite.cpp
+++ LoLSuite.cpp
@@ -332,7 +332,7 @@ void manageTasks(const std::wstring& task)
 			L"winget uninstall Oracle.JDK.19 --purge -h",
 			L"winget uninstall Oracle.JDK.18 --purge -h",
 			L"winget uninstall Oracle.JDK.17 --purge -h",
-			L"winget install Mojang.MinecraftLauncher",
+			L"winget install Mojang.MinecraftLauncher --accept-package-agreements",
 			L"winget install Oracle.JDK.23 --accept-package-agreements",
 		};
 		executeCommands(Commands);
